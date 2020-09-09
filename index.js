e1662ad3e0a85b57b89b5ba1764b0970e4a36768
gameinn <42845692+x201s@users.noreply.github.com>
2020-09-09 13:33:41 -0500

    Final index.js
    Final stable index.js before attempting to export modules to seperate .js file

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const { Client, MessageAttachment } = require('discord.js');
 
 const prefix = ".";
 
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
 client.on("message", function(message) { 
     if (message.author.bot) return; 
     if (!message.content.startsWith(prefix)) return;
@@ -21,49 +25,21 @@ client.on("message", function(message) {
     else if (command === "sum") {
         const numArgs = args.map(x => parseFloat(x));
         const sum = numArgs.reduce((counter, x) => counter += x);
-        message.reply(`Total sum: ${sum}!`);
+        message.channel.send(`Total sum: ${sum}!`);
     }
 
     else if (command === "avatar") {
-        message.reply(message.author.displayAvatarURL({dynamic:true,size:4096}));
-    }
+      message.channel.send(message.mentions.users.first().displayAvatarURL({dynamic:true,size:4096}));
+  }
 
-    else if (command === "h") {
-        message.reply(`
-        .ping: Check latency
-        .sum: Display total sum
-        .avatar: Display user pfp
-        .restore: Default index.js file
-        .rps: rock paper scissors / usage: .rps <r,p,s>
-        .bf: betflip / usage: .bf h or .bf t
-        `)
-    }
+  else if (command === "a") {
+    message.channel.send(message.author.displayAvatarURL({dynamic:true,size:4096}));
+}
 
-    else if (command === "fetch-2k13") {
-        const attachment = new Discord.MessageAttachment('https://cdn.discordapp.com/attachments/739885794780643378/739894276393271386/NBA_2K13_ULUS105982k18PG.zip');
-        message.channel.send(attachment);
-    }
-
-    else if (command === "fetch-shield") {
-        const attachment = new Discord.MessageAttachment('https://cdn.discordapp.com/attachments/739885917757636701/740611996264824913/mearly_clean-endgame.zip');
-        message.channel.send(attachment);
-    }
-
-    else if (command === "hb-bro") {
-        const attachment = new Discord.MessageAttachment('https://media.giphy.com/media/SUA9E0GcSyhCU/giphy.gif');
-        message.channel.send(attachment);
-    }
-
-    else if (command === "restore") {
-        const attachment = new Discord.MessageAttachment('https://cdn.discordapp.com/attachments/750893611062657095/751169341885513828/index.zip');
-        message.channel.send(attachment);
-    }
-
-    else if (command === "smug") {
-        const attachment = new Discord.MessageAttachment('https://cdn.discordapp.com/attachments/457008618110451722/745677591855366174/eeveewatersmug.png');
-        message.channel.send(attachment);
-    }
-
+else if (message.content === `${prefix}server`) {
+	message.channel.send(`Server name: ${message.guild.name}`);
+}
+  
     else if (command == 'bf') {
         const acceptedReplies = ['h', 't'];
         const random = Math.floor((Math.random() * acceptedReplies.length));
@@ -120,15 +96,75 @@ client.on("message", function(message) {
         }
       }
 
-      else if (command == 'fetch-emerald') {
+      else if (command == 'games') {
         const exampleEmbed = {
           color: 0x0099ff,
-          title: 'Pokemon Emerald',
-          url: 'https://cdn.discordapp.com/attachments/739885704041201826/739904243644760154/pkmne.zip',
-          author: {
-
-            icon_url: 'https://cdn.discordapp.com/attachments/457008618110451722/750872636665364500/Z.png',
+          description: 'BuntaBot is an experimental discord bot. ',
+          thumbnail: {
+            url: 'https://cdn.discordapp.com/attachments/751691556691640400/751691738997063750/watercup.gif',
           },
+          fields: [
+            {
+              name: '.snes',
+              value: 'Super Nintendo game save commands',
+            },
+            {
+              name: '.gba',
+              value: 'Game Boy Advance game save commands',
+            },
+            {
+              name: '.psx',
+              value: 'Playstation 1 game save commands',
+            },
+            {
+              name: '.ds',
+              value: 'Nintendo DS game save commands',
+            },
+            {
+              name: '.3ds',
+              value: 'Nintendo 3DS game save commands',
+            },
+            {
+              name: '.switch',
+              value: 'Nintendo Switch game save commands',
+            },
+          ],
+        };
+    
+        message.channel.send({ embed: exampleEmbed });
+    
+      }
+
+      else if (command == 'base-0100959010466000') {
+        const exampleEmbed = {
+          color: 0x0099ff,
+          title: 'Hypnospace Outlaw [0100959010466000]',
+          url: 'https://drive.google.com/file/d/1_fe12OgqvZhn_sr1VkKfAB5ntSoVuWaf/view?usp=sharing',
+          description: 'Greetings Enforcer, and thank you for enlisting in the Hypnospace Patrol Department! As the world falls into its slumber, Hypnospace becomes a bustling global village.',
+          thumbnail: {
+            url: 'https://img-eshop.cdn.nintendo.net/i/1d3821b3e9e03ee4e03eb04f966f222f40f2cfb3c626f674ea93ab9f074bfb95.jpg',
+          },
+          fields: [
+            {
+              name: 'Release Date',
+              value: '8/28/20',
+            },
+            {
+              name: 'Size',
+              value: '565,49 MB',
+            },
+          ],
+        };
+    
+        message.channel.send({ embed: exampleEmbed });
+    
+      }
+
+      else if (command == 'gba-agb-bpee-usa') {
+        const exampleEmbed = {
+          color: 0x0099ff,
+          title: 'Pokemon Emerald [agb-bpee-usa]',
+          url: 'https://cdn.discordapp.com/attachments/739885704041201826/751823074638168134/Pokemon_Emerald_AGB-BPEE-USA.zip',
           description: 'Pokémon Emerald Version is a role-playing video game developed by Game Freak, published by The Pokémon Company and Nintendo for the Game Boy Advance. It was first released in Japan in 2004, and was later released internationally in 2005. It is an enhanced version of Pokémon Ruby and Sapphire and is part of the third generation of the Pokémon video game series. ',
           thumbnail: {
             url: 'https://upload.wikimedia.org/wikipedia/en/f/f7/PokemonEmeraldBox.jpg',
@@ -144,6 +180,73 @@ client.on("message", function(message) {
         message.channel.send({ embed: exampleEmbed });
     
       }
+
+      else if (command == 'h') {
+        const exampleEmbed = {
+          color: 0x0099ff,
+          description: 'BuntaBot is an experimental discord bot. ',
+          thumbnail: {
+            url: 'https://cdn.discordapp.com/attachments/751691556691640400/751691738997063750/watercup.gif',
+          },
+          fields: [
+            {
+              name: '.h',
+              value: 'Display helpful commands',
+            },
+            {
+              name: '.hb',
+              value: 'Display homebrew commands',
+            },
+            {
+              name: '.games',
+              value: 'Display game saves commands',
+            },
+            {
+              name: '.e',
+              value: 'Display emotion commands',
+            },
+            {
+              name: '.avatar',
+              value: 'Generate a link of users avatar',
+            },
+            {
+              name: '.ping',
+              value: 'Check API latency',
+            },
+            {
+              name: '.bf',
+              value: 'Betflip - Usage ex. (.bf h) or (.bf t)',
+            },
+          ],
+        };
+    
+        message.channel.send({ embed: exampleEmbed });
+      }
+
+      else if (command === `keys`) {
+        message.channel.send(`https://cdn.discordapp.com/attachments/648763453195943946/671865304959287306/keys_9.1.0_v2.txt`)
+      }
+
+      else if (command === `atmos`) {
+        message.channel.send(`https://github.com/Atmosphere-NX/Atmosphere/releases`)
+      }
+
+      else if (command === `sigpatches`) {
+        message.channel.send(`https://github.com/eXhumer/patches/releases`)
+      }
+
+      else if (command === `nut`) {
+        message.channel.send(`https://github.com/blawar/nut/releases`)
+      }
+
+      else if (command === `nsz`) {
+        message.channel.send(`https://github.com/nicoboss/nsz/releases`)
+      }
+
+      else if (command === "hug") {
+        const attachment = new Discord.MessageAttachment('https://i.imgur.com/TYkINci.gif');
+        message.channel.send(attachment);
+      }
 });      
 
-client.login(config.BOT_TOKEN);
+client.login(config.token);
